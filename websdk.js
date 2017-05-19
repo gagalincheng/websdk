17d201e665d8f9cdff0383ee1feeb65aa8e605a3
gagalincheng <gagalincheng@gmail.com>
2017-05-19 10:35:17 +0800

    sdk version:v1.3 update time:2017-05-19

--- websdk.js
+++ websdk.js
@@ -1,5 +1,108 @@
-/*sdk version:v1.2.2 update time:2017-02-15*/
+/*sdk version:v1.3 update time:2017-05-19*/
 (function() {
+	var JSON = !window.JSON ?
+	function() {
+		function f(n) {
+			return n < 10 ? "0" + n: n
+		}
+		Date.prototype.toJSON = function() {
+			return this.getUTCFullYear() + "-" + f(this.getUTCMonth() + 1) + "-" + f(this.getUTCDate()) + "T" + f(this.getUTCHours()) + ":" + f(this.getUTCMinutes()) + ":" + f(this.getUTCSeconds()) + "Z"
+		};
+		var m = {
+			"\b": "\\b",
+			"\t": "\\t",
+			"\n": "\\n",
+			"\f": "\\f",
+			"\r": "\\r",
+			'"': '\\"',
+			"\\": "\\\\"
+		};
+		function stringify(value, whitelist) {
+			var a, i, k, l, r = /["\\\x00-\x1f\x7f-\x9f]/g,
+			v;
+			switch (typeof value) {
+			case "string":
+				return r.test(value) ? '"' + value.replace(r,
+				function(a) {
+					var c = m[a];
+					if (c) {
+						return c
+					}
+					c = a.charCodeAt();
+					return "\\u00" + Math.floor(c / 16).toString(16) + (c % 16).toString(16)
+				}) + '"': '"' + value + '"';
+			case "number":
+				return isFinite(value) ? String(value) : "null";
+			case "boolean":
+			case "null":
+				return String(value);
+			case "object":
+				if (!value) {
+					return "null"
+				}
+				if (typeof value.toJSON === "function") {
+					return stringify(value.toJSON())
+				}
+				a = [];
+				if (typeof value.length === "number" && !(value.propertyIsEnumerable("length"))) {
+					l = value.length;
+					for (i = 0; i < l; i += 1) {
+						a.push(stringify(value[i], whitelist) || "null")
+					}
+					return "[" + a.join(",") + "]"
+				}
+				if (whitelist) {
+					l = whitelist.length;
+					for (i = 0; i < l; i += 1) {
+						k = whitelist[i];
+						if (typeof k === "string") {
+							v = stringify(value[k], whitelist);
+							if (v) {
+								a.push(stringify(k) + ":" + v)
+							}
+						}
+					}
+				} else {
+					for (k in value) {
+						if (typeof k === "string") {
+							v = stringify(value[k], whitelist);
+							if (v) {
+								a.push(stringify(k) + ":" + v)
+							}
+						}
+					}
+				}
+				return "{" + a.join(",") + "}"
+			}
+		}
+		return {
+			stringify: stringify,
+			parse: function(text, filter) {
+				var j;
+				function walk(k, v) {
+					var i, n;
+					if (v && typeof v === "object") {
+						for (i in v) {
+							if (Object.prototype.hasOwnProperty.apply(v, [i])) {
+								n = walk(i, v[i]);
+								if (n !== undefined) {
+									v[i] = n
+								} else {
+									delete v[i]
+								}
+							}
+						}
+					}
+					return filter(k, v)
+				}
+				if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, "@").replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, "]").replace(/(?:^|:|,)(?:\s*\[)+/g, ""))) {
+					j = eval("(" + text + ")");
+					return typeof filter === "function" ? walk("", j) : j
+				}
+				throw new SyntaxError("parseJSON")
+			}
+		}
+	} () : window.JSON;
 	window.hiidoEvent = function(prodid, eventid) {
 		var _xc = this.xc + "&prodid=" + prodid + "&eventid=" + eventid;
 		var sec;
@@ -52,6 +155,9 @@
 		this.setActtype = function(value) {
 			_xc += "&act_type=" + value
 		};
+		this.setTpid = function(value) {
+			_xc += "&tpid=" + value
+		};
 		this.setStatisobj = function(value) {
 			_xc += "&statis_obj=" + value
 		};
@@ -102,7 +208,8 @@
 			var guid = this.ui + ut;
 			var time = parseInt(new Date().getTime() / 1000);
 			var img = new Image(1, 1);
-			img.src = this._hiidoHttpProtocol + "://" + this._hiidoHttpHost + "/j.gif?" + _xc + "&uid=" + this.uid + "&guid=" + guid + "&time=" + time + "&value=" + value;
+			img.src = this._hiidoHttpProtocol + "//" + this._hiidoHttpHost + "/j.gif?" + _xc + "&uid=" + this.uid + "&guid=" + guid + "&time=" + time + "&value=" + value;
+			console.log(img.src);
 			img.onload = function() {
 				return
 			};
@@ -117,7 +224,9 @@
 		this.reportLeave = function() {
 			var value = (reporttype == "heart") ? sec: val;
 			this.report(value);
-			if (reporttype == "heart") clearInterval(heartTimer)
+			if (reporttype == "heart") {
+				clearInterval(heartTimer)
+			}
 		};
 		this.countSec = function() {
 			sec = sec + 1
@@ -125,7 +234,7 @@
 	};
 	hiidoEvent.prototype = {
 		constructor: hiidoEvent,
-		_hiidoHttpProtocol: "https",
+		_hiidoHttpProtocol: window.location.protocol,
 		_hiidoHttpHost: "ylog.hiido.com",
 		uid: getCookie("yyuid"),
 		ui: getUi(),
